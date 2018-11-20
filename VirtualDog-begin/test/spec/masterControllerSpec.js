1b48db75d1c04dc33847d80ed2570d0abdbd046d
Nuno Costa <nunofilipe.costa@gmail.com>
2018-11-20 13:00:30 +0000

    first test

---
+++ VirtualDog-begin/test/spec/masterControllerSpec.js
@@ -0,0 +1,8 @@
+/// <reference path="../../typings/index.d.ts" />
+describe('Hello World', function () {
+    it('my first test', function () {
+        console.log('Hello Wolrd');
+        expect(true).toBeTruthy('Something true is not truthy');
+    });
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFzdGVyQ29udHJvbGxlclNwZWMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJtYXN0ZXJDb250cm9sbGVyU3BlYy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxpREFBaUQ7QUFFakQsUUFBUSxDQUFDLGFBQWEsRUFBRTtJQUN0QixFQUFFLENBQUMsZUFBZSxFQUFFO1FBQ2xCLE9BQU8sQ0FBQyxHQUFHLENBQUMsYUFBYSxDQUFDLENBQUM7UUFDM0IsTUFBTSxDQUFDLElBQUksQ0FBQyxDQUFDLFVBQVUsQ0FBQyw4QkFBOEIsQ0FBQyxDQUFDO0lBQzFELENBQUMsQ0FBQyxDQUFDO0FBQ0wsQ0FBQyxDQUFDLENBQUMifQ==
---
+++ VirtualDog-begin/test/spec/masterControllerSpec.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../typings/index.d.ts" />
+
+describe('Hello World', () => {
+  it('my first test', () => {
+    console.log('Hello Wolrd');
+    expect(true).toBeTruthy('Something true is not truthy');
+  });
+});
